31affff95f9bf995b40f8beb61dc5157ae52896f
John Kaster <john.kaster@looker.com>
2018-01-05 13:09:30 -0800

    ViewSchema join columns are now de-duped when adding
    Aliases are merged to the existing column when a duplicate is found

--- src/main/kotlin/com/looker/sql_query_parser/parser/ViewSchema.kt
+++ src/main/kotlin/com/looker/sql_query_parser/parser/ViewSchema.kt
@@ -3,6 +3,7 @@ package com.looker.sql_query_parser.parser
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.tree.ParseTree
+import org.antlr.v4.runtime.tree.TerminalNodeImpl
 import java.io.ByteArrayInputStream
 
 fun ParseTree.getChildren() : List<ParseTree> {
@@ -10,44 +11,133 @@ fun ParseTree.getChildren() : List<ParseTree> {
     return list.filter({tree -> true})
 }
 
-class Aliases(aList: MutableList<String> = mutableListOf()) : MutableList<String> by aList
+class Aliases(aList: MutableSet<String> = mutableSetOf()) : MutableSet<String> by aList
 
-data class Column(val name: String, var aliases: Aliases = Aliases()) {
+data class Column(var originalName: String, var aliases: Aliases = Aliases()) {
+    var database : String = ""
+    var tableName : String = ""
+    var name : String = ""
+    init {
+        val parts = originalName.split('.')
+        when (parts.size) {
+            1 -> name = originalName
+            2 -> {
+                tableName = parts[0]
+                name = parts[1]
+            }
+            3 -> {
+                database = parts[0]
+                tableName = parts[1]
+                name = parts[2]
+            }
+        }
+    }
+    var table: Table? = null
     fun addAlias(alias : String) : Boolean = aliases.add(alias)
+    fun fullName() : String {
+        if (database.isBlank()) {
+            if (tableName.isBlank()) return name
+            return "${tableName}.${name}"
+        }
+        return "${database}.${tableName}.${name}"
+    }
 }
 
-class Columns(aList: MutableList<Column> = mutableListOf()): MutableList<Column> by aList
+class Columns(aList: MutableSet<Column> = mutableSetOf()): MutableSet<Column> by aList {
+    fun find(nameOrAlias: String) : Column? {
+        return this.firstOrNull { col -> col.fullName().equals(nameOrAlias, true)
+                || col.originalName.equals(nameOrAlias,true)
+                || col.aliases.any { alias -> alias.equals(nameOrAlias, true) }
+        }
+    }
 
-data class Table(val name: String, var columns: MutableList<Column> = mutableListOf(), var aliases: MutableList<String> = mutableListOf()) {
-    fun addColumn(column: Column) : Boolean = columns.add(column)
+    fun find(column: Column) : Column? {
+        var found = find(column.originalName)
+        if (found == null) found = find(column.fullName())
+        if (found != null) column.aliases.forEach{ alias -> found.aliases.add(alias) } // add any missing aliases
+        return found
+    }
+}
+
+data class Table(var originalName: String, var columns: Columns = Columns(),
+                 var aliases: Aliases = Aliases()) {
+    var database : String = ""
+    var name : String = ""
+    init {
+        val parts = originalName.split('.')
+        when (parts.size) {
+            1 -> name = originalName
+            2 -> {
+                database = parts[0]
+                name = parts[1]
+            }
+        }
+    }
+
+    fun fullName() : String {
+        if (database.isBlank()) return name
+        return database + "." + name
+    }
+
+    fun addColumn(column: Column) : Column {
+        var found = columns.find(column)
+        if (found == null) {
+            found = column
+            columns.add(found)
+        }
+        return found
+    }
     fun addAlias(alias: String) : Boolean = aliases.add(alias)
 }
 
-class Tables(aList: MutableList<Table> = mutableListOf()): MutableList<Table> by aList
+class Tables(aList: LinkedHashSet<Table> = linkedSetOf()): MutableSet<Table> by aList {
+    fun find(nameOrAlias: String) : Table? {
+        return this.firstOrNull { table -> table.name.equals(nameOrAlias,true) ||
+                table.aliases.any { alias -> alias.equals(nameOrAlias, true) }
+        }
+    }
+}
 
 // Enum based on this great post https://stackoverflow.com/a/17946222/74137
 enum class JoinType {
     INNER,
-    LEFT_OUTER,
-    RIGHT_OUTER,
+    OUTER,
     FULL_OUTER,
     SELF,
     CROSS,
     NATURAL
 }
 
-data class Join(val fromTable: Table, val toTable: Table, val fromKey: Column, val toKey: Column, val joinType: JoinType)
-
-class Joins(aList: MutableList<Join> = mutableListOf()): MutableList<Join> by aList
-
-data class ViewSchema(val name: String, var tables: Tables = Tables(), var columns : Columns = Columns(), var joins: Joins = Joins()) {
-    fun addTable(table: Table) : Boolean = tables.add(table)
-    fun addJoin(join: Join) : Boolean = joins.add(join)
-    fun addColumn(column: Column) : Boolean = columns.add(column)
+data class Join(val table: Table, val leftColumn: Column, val rightColumn: Column,
+                val inner: Boolean = true, val left: Boolean = true, var type : JoinType = JoinType.INNER) {
+    val outer = !inner
+    val right = !left
 }
 
-class ViewExtractor(val sql: String) {
-    var viewSchema = ViewSchema("top")
+class Joins(aList: MutableSet<Join> = mutableSetOf()): MutableSet<Join> by aList {
+    fun find(nameOrAlias: String) : Join? {
+        return this.firstOrNull { col -> col.table.name.equals(nameOrAlias,true) ||
+                col.table.aliases.any { alias -> alias.equals(nameOrAlias, true) }
+        }
+    }
+
+}
+
+data class ViewSchema(val sql: String, var tables: Tables = Tables(), var columns : Columns = Columns(),
+                      var joins: Joins = Joins()) {
+    var ast : MySQLParser.RootContext
+
+    private var leftJoin = true
+    private var innerJoin = true
+    private var joinTable : Table? = null
+    private var leftColumn : Column? = null
+    private var rightColumn : Column? = null
+
+    init {
+        ast = ParseSql(sql)
+        processNodes(ast.children)
+        resolve()
+    }
 
     companion object {
         fun ParseSql(sql: String) : MySQLParser.RootContext {
@@ -59,9 +149,9 @@ class ViewExtractor(val sql: String) {
 
             val parser = MySQLParser(tokenStream)
             // use error listener to throw exception when parser fails (as opposed to just printing to console)
-            parser.removeErrorListeners();
+            parser.removeErrorListeners()
             parser.addErrorListener(ThrowingErrorListener.INSTANCE)
-            lexer._input = caseStream // set back to original case?
+            lexer._input = caseStream // set back to original case
 
             return parser.root()
         }
@@ -74,34 +164,81 @@ class ViewExtractor(val sql: String) {
         }
     }
 
-    fun select(node: ParseTree) {
-        node.getChildren().forEach { item ->
-            when (item) {
-                is MySQLParser.SelectElementsContext -> selectColumns(item)
-                is MySQLParser.FromClauseContext -> selectFrom(item)
-                is MySQLParser.JoinPartContext -> join(item)
-                else -> select(item)
+    fun addTable(table: Table) : Table {
+        // TODO handle addition error
+        tables.add(table)
+        return table
+    }
+
+    fun addJoin(join: Join) : Join {
+        // TODO handle addition error
+        joins.add(join)
+        return join
+    }
+
+    private fun findTableForColumn(column: Column) {
+        val defaultTable = tables.firstOrNull()
+        if (defaultTable == null) return
+        if (column.table == null) {
+            val table = if (!column.tableName.isBlank()) tables.find(column.tableName)
+            else {
+                // TODO 1. look for a column name in one of the SELECT tables that matches the column name
+                // if that's not found, assign the column's table to the default table
+                defaultTable
+            }
+            if (table != null) {
+                table.addColumn(column)
+                column.table = table
+                column.tableName = table.name
             }
         }
 
     }
 
-    private fun selectTable(node: ParseTree) {
+    fun addColumn(column: Column) : Column {
+        // TODO handle addition error
+        findTableForColumn(column)
+        var found = columns.find(column)
+        if (found == null) {
+            columns.add(column)
+            found = column
+        }
+        return found
+    }
+
+    private fun resolve() {
+        columns.forEach { column ->
+            findTableForColumn(column)
+        }
+    }
+
+    private fun select(node: ParseTree) {
+        node.getChildren().forEach { item ->
+            when (item) {
+                is MySQLParser.SelectElementsContext -> selectColumns(item)
+                is MySQLParser.FromClauseContext -> selectFrom(item)
+                else -> select(item)
+            }
+        }
+    }
+
+    private fun selectTable(node: ParseTree) : Table {
         var name = ""
-        var aliases = Aliases()
+        val aliases = Aliases()
         node.getChildren().forEach { item ->
             when (item) {
                 is MySQLParser.TableNameContext -> name = item.text
                 is MySQLParser.UidContext -> aliases.add(item.text)
             }
         }
-        viewSchema.addTable(Table(name, aliases = aliases))
+        return addTable(Table(name, aliases = aliases))
     }
 
     private fun selectFrom(node: ParseTree) {
         node.getChildren().forEach { item ->
             when (item) {
                 is MySQLParser.AtomTableItemContext -> selectTable(item)
+                is MySQLParser.JoinPartContext -> join(item)
                 else -> selectFrom(item)
             }
         }
@@ -109,47 +246,92 @@ class ViewExtractor(val sql: String) {
 
     private fun selectColumn(node: ParseTree) : Column {
         var name = ""
-        var aliases = Aliases()
+        val aliases = Aliases()
         node.getChildren().forEach { item ->
             when (item) {
                 is MySQLParser.FullColumnNameContext -> name = item.text
                 is MySQLParser.UidContext -> aliases.add(item.text)
             }
         }
-        return Column(name, aliases)
+        return addColumn(Column(name, aliases))
     }
 
     private fun selectColumns(node: ParseTree) {
         node.getChildren().forEach { item ->
             when (item) {
-                is MySQLParser.SelectColumnElementContext -> viewSchema.addColumn(selectColumn(item))
+                is MySQLParser.SelectColumnElementContext -> selectColumn(item)
                 else -> selectColumns(item)
             }
         }
     }
 
-    private fun naturalJoin(join: MySQLParser.NaturalJoinContext) {
-
+    private fun joinCompare(node: ParseTree) {
+        var column : Column
+        node.getChildren().forEach { item ->
+            when (item) {
+                is MySQLParser.FullColumnNameExpressionAtomContext -> {
+                    column = selectColumn(item)
+                    if (leftColumn == null) leftColumn = column
+                    else rightColumn = column
+                }
+                else -> joinCompare(item)
+            }
+        }
     }
 
-    private fun outerJoin(join: MySQLParser.OuterJoinContext) {
-
+    private fun joinNode(node: MySQLParser.JoinPartContext) : Join {
+        node.getChildren().forEach { item ->
+            when (item) {
+                is TerminalNodeImpl -> when (item.text) {
+                    "RIGHT" -> leftJoin = false
+                    "OUTER" -> innerJoin = false
+                }
+                is MySQLParser.AtomTableItemContext -> joinTable = selectTable(item)
+                is MySQLParser.PredicateExpressionContext -> joinCompare(item)
+            }
+        }
+        return Join(joinTable!!, leftColumn!!, rightColumn!!, innerJoin, leftJoin)
     }
 
-    private fun straightJoin(join: MySQLParser.StraightJoinContext) {
-
+    private fun initJoin() {
+        joinTable = null
+        leftColumn = null
+        rightColumn = null
+        innerJoin = true
+        leftJoin = true
     }
 
-    private fun innerJoin(join: MySQLParser.InnerJoinContext) {
+    private fun naturalJoin(node: MySQLParser.NaturalJoinContext) : Join {
+        val join = joinNode(node)
+        join.type = JoinType.NATURAL
+        return join
+    }
 
+    private fun outerJoin(node: MySQLParser.OuterJoinContext) : Join {
+        val join = joinNode(node)
+        join.type = JoinType.OUTER
+        return join
+    }
+
+    private fun straightJoin(node: MySQLParser.StraightJoinContext) : Join {
+        val join = joinNode(node)
+        join.type = JoinType.INNER
+        return join
+    }
+
+    private fun innerJoin(node: MySQLParser.InnerJoinContext) : Join {
+        val join = joinNode(node)
+        join.type = JoinType.INNER
+        return join
     }
 
     private fun join(node: ParseTree) {
+        initJoin()
         when (node) {
-            is MySQLParser.NaturalJoinContext -> naturalJoin(node)
-            is MySQLParser.OuterJoinContext -> outerJoin(node)
-            is MySQLParser.StraightJoinContext -> straightJoin(node)
-            is MySQLParser.InnerJoinContext -> innerJoin(node)
+            is MySQLParser.NaturalJoinContext -> addJoin(naturalJoin(node))
+            is MySQLParser.OuterJoinContext -> addJoin(outerJoin(node))
+            is MySQLParser.StraightJoinContext -> addJoin(straightJoin(node))
+            is MySQLParser.InnerJoinContext -> addJoin(innerJoin(node))
             else -> return // Throw error here
         }
     }
@@ -160,12 +342,4 @@ class ViewExtractor(val sql: String) {
             else processNodes(node.getChildren())
         }
     }
-
-    fun analyze() : ViewSchema {
-        viewSchema = ViewSchema("top")
-        var root = ParseSql(sql)
-        processNodes(root.children)
-        return viewSchema
-    }
-
 }
--- src/test/kotlin/SimpleTest.kt
+++ src/test/kotlin/SimpleTest.kt
@@ -1,6 +1,6 @@
 
 import com.looker.sql_query_parser.parser.MySQLParser
-import com.looker.sql_query_parser.parser.ViewExtractor
+import com.looker.sql_query_parser.parser.ViewSchema
 import com.looker.sql_query_parser.parser.getChildren
 import org.antlr.v4.runtime.tree.ParseTree
 import org.junit.Assert
@@ -8,8 +8,10 @@ import org.junit.Test
 
 class SimpleTest {
 
+    val sql = "SELECT birthdate, u.id userid, u.username, o.id orderid, o.orderdate, p.id partid, p.partname FROM users u JOIN orders o ON u.id = o.user_id RIGHT OUTER JOIN parts p ON p.id = o.part_id"
+
     fun preserveCase(sql: String) : MySQLParser.RootContext {
-        val root = ViewExtractor.ParseSql(sql)
+        val root = ViewSchema.ParseSql(sql)
         val source = root.getStart().tokenSource.inputStream.toString()
         Assert.assertEquals("Statement casing should be preserved in inputStream", sql, source)
         return root
@@ -17,15 +19,15 @@ class SimpleTest {
 
     @Test fun `tests simple select`() {
         val root = preserveCase("SELECT * FROM USERS")
+        Assert.assertNotNull(root)
     }
 
     @Test fun `tests single join`() {
         val root = preserveCase("SELECT * FROM USERS U JOIN ORDERS O ON U.ID = O.USER_ID")
+        Assert.assertNotNull(root)
     }
 
     @Test fun `tests and displays hierarchy for multiple joins`() {
-        val sql = "SELECT birthdate, u.id userid, u.username, o.id orderid, o.orderdate, p.id partid, p.partname FROM users u LEFT OUTER JOIN orders o ON u.id = o.user_id RIGHT OUTER JOIN part p ON p.id = p.part_id"
-//        val sql = "SELECT * FROM USERS U JOIN ORDERS O ON U.ID = O.USER_ID JOIN PART P ON P.ID = P.PART_ID"
         val root = preserveCase(sql)
         Assert.assertEquals("Should be 2 children", 2, root.childCount)
         family(root.children)
@@ -39,10 +41,51 @@ class SimpleTest {
     }
 
     @Test fun `extracts viewSchema`() {
-        val sql = "SELECT birthdate, u.id userid, u.username, o.id orderid, o.orderdate, p.id partid, p.partname FROM users u LEFT OUTER JOIN orders o ON u.id = o.user_id RIGHT OUTER JOIN part p ON p.id = p.part_id"
-        val extractor = ViewExtractor(sql)
-        val view = extractor.analyze()
-        Assert.assertEquals("Column count",7, view.columns.size)
-        Assert.assertEquals("Table count", 1, view.tables.size)
+        val schema = ViewSchema(sql)
+        Assert.assertEquals("Column count",9, schema.columns.size)
+        Assert.assertEquals("Table count", 3, schema.tables.size)
+        Assert.assertEquals("Join count", 2, schema.joins.size)
+        
+        val tableUsers = schema.tables.first()
+        Assert.assertNotNull("Users table exists", tableUsers)
+        Assert.assertEquals("Expected USERS table", "USERS", tableUsers.name)
+        Assert.assertTrue("User alias 'U' exists", tableUsers.aliases.any { alias -> alias == "U"})
+        Assert.assertEquals( "User table column count", 3, tableUsers.columns.size)
+        Assert.assertTrue( "All columns have full table name", tableUsers.columns.all { col -> col.tableName.equals(tableUsers.name)})
+
+        val userid = schema.columns.find("userid")
+        Assert.assertNotNull("UserID alias column found", userid)
+        Assert.assertEquals("UserID Table is USERS", tableUsers, userid!!.table)
+        Assert.assertEquals("USERS.ID", userid.fullName())
+
+        val tableOrders = schema.tables.find("orders")
+        Assert.assertNotNull("Orders table exists", tableOrders)
+        Assert.assertEquals("Expected Orders table", "ORDERS", tableOrders!!.name)
+        Assert.assertTrue("Orders alias 'O' exists", tableOrders.aliases.any { alias -> alias.equals("O", true)})
+        Assert.assertEquals( "Orders table column count", 4, tableOrders.columns.size)
+        Assert.assertTrue( "All columns have full table name", tableOrders.columns.all { col -> col.tableName == tableOrders.name})
+
+        val tableParts = schema.tables.find("parts")
+        Assert.assertNotNull("Parts table exists", tableParts)
+        Assert.assertEquals("Expected Parts table", "PARTS", tableParts!!.name)
+        Assert.assertTrue("Parts alias 'P' exists", tableParts.aliases.any { alias -> alias.equals("P", true)})
+        Assert.assertEquals( "Parts table column count", 2, tableParts.columns.size)
+        Assert.assertTrue( "All columns have full table name", tableParts.columns.all { col -> col.tableName == tableParts.name})
+
+        val joinOrders = schema.joins.find("orders")
+        Assert.assertNotNull("Orders join exists", joinOrders)
+        Assert.assertEquals("Orders table name", "ORDERS", joinOrders!!.table.name)
+        Assert.assertTrue("Orders is LEFT join", joinOrders.left)
+        Assert.assertTrue("Orders is INNER join", joinOrders.inner)
+        Assert.assertEquals("USERS.ID", joinOrders.leftColumn.fullName())
+        Assert.assertEquals("ORDERS.USER_ID", joinOrders.rightColumn.fullName())
+
+        val joinParts = schema.joins.find("parts")
+        Assert.assertNotNull("Parts join exists", joinParts)
+        Assert.assertEquals("Parts table name", "PARTS", joinParts!!.table.name)
+        Assert.assertTrue("Parts is RIGHT join", joinParts.right)
+        Assert.assertTrue("Parts is OUTER join", joinParts.outer)
+        Assert.assertEquals("PARTS.ID", joinParts.leftColumn.fullName())
+        Assert.assertEquals("ORDERS.PART_ID", joinParts.rightColumn.fullName())
     }
 }
